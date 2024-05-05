5af28356f3e0985fc7434b0f4f8ce6e41ec0a5b3
ErnestoMeza <ernestogarcia6055@gmail.com>
2024-05-05 12:37:12 -0600

    Trabajos hechos

---
+++ Guia3EntregarDomingo/Guia3EntregarDomingo/Empleado.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Guia3EntregarDomingo
+{
+    internal class Empleado
+    {
+        private string nombre;
+        private int numeroEmpleado ;
+        private decimal salario;
+        private bool activo ;
+        //creamos un metodo que obtendra los private para no tener el error en la clase Empleado
+        public Empleado(string nombre, int numeroEmpleado, decimal salario) {
+            this.nombre = nombre;
+            this.numeroEmpleado = numeroEmpleado;
+            this.salario = salario;
+            this.activo = true;
+        }
+
+        //creamos el metodo MostrarDetalles obtenemos el nombre, numempleado,salario y su esatdo
+        public void MostrarDetalles() {
+
+            string estado = (activo) ? "Activo" : "Inactivo";
+            Console.WriteLine($"Nombre: {nombre}, Numero de empleado: {numeroEmpleado}, salario: {salario:c}, estado{estado}");
+        }
+        //obtenemos el numero de empleado 
+        public int ObtenerNumeroEmpleado()
+        {
+            return this.numeroEmpleado;
+        }
+        //creamos un metodo con parametro para cambiar el estado del empleado si esta acivado o desactivado activo e inactivo
+        public void CambiarEstado(int nuevoEstado)
+        {
+            if (nuevoEstado == 1)
+            {
+                activo = true;
+                Console.WriteLine("Empleado activado");
+            }
+            else if (nuevoEstado == 0)
+            {
+                activo = false;
+                Console.WriteLine("Empleado desactivado");
+            }
+            else {
+
+                Console.WriteLine("Estado no valido. ");
+            }
+
+        } 
+
+
+    }
+}
+       
+    
+
---
+++ Guia3EntregarDomingo/Guia3EntregarDomingo/Program.cs
@@ -0,0 +1,53 @@
+﻿using System.Linq.Expressions;
+using System.Threading.Channels;
+
+namespace Guia3EntregarDomingo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //creamos una lista de los 3 empleados que tenemos
+            List<Empleado> listaEmpleados = new List<Empleado>();
+
+            listaEmpleados.Add(new Empleado("Juan", 101, 30000));
+            listaEmpleados.Add(new Empleado("Maria", 102, 35000));
+            listaEmpleados.Add(new Empleado("Pedro", 103, 32000));
+            // nos ayudamos de un auxiliar
+            foreach (var empleado in listaEmpleados)
+            {
+                empleado.MostrarDetalles();// llamamos el metodo mostrar detalles que obtiene los nombres,numempleado y su salario
+            }
+            //cambiamos el estado del empleado si queremos que sea activo o inactivo 
+            int numeroEmpleadoCambiarEstado = 103;
+            Empleado empleadoCambiarEstado = BuscarEmpleado(listaEmpleados, numeroEmpleadoCambiarEstado);
+            
+            if (empleadoCambiarEstado != null)
+            {
+                empleadoCambiarEstado.CambiarEstado(1);
+                empleadoCambiarEstado.MostrarDetalles();
+            }
+            else// si llega a tener otro numero que no sea el de el no lo encontrara
+            {
+                Console.WriteLine($"Empleado con número {numeroEmpleadoCambiarEstado} no encontrado. ");
+            }
+
+        }//el metodo nos ayuda para buscar el empleado con el numero del empleado
+        static Empleado BuscarEmpleado(List<Empleado> empleados, int numeroEmpleado)
+        {
+            foreach (var empleado in empleados)
+            {
+
+                if (empleado.ObtenerNumeroEmpleado() == numeroEmpleado)
+                {
+
+                    return empleado;
+                }
+               
+            }
+            return null;
+        }
+
+
+    }
+}
