e682447223ab438f897bec1506deb94e652b6495
ErnestoMeza <ernestogarcia6055@gmail.com>
2024-05-12 18:10:16 -0600

    sistematico finalizado

---
+++ Automoviles/Automoviles/Automovil.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Automoviles
+{
+    public partial class Automovil
+    {
+
+        public string Marca { get; set; } = "Toyota";
+        public string Placa { get; set; }= "M12542";
+        public string Modelo { get; set; } = "Supra";
+        public string Color { get; set; } = "Rojo";
+        public int CapacidadTanqueCombustible { get; set; } = 100;
+        public double NivelCombustible { get; set; } = 0;
+        public int RendimientoCombustible { get; set; } = 900;
+
+        public Automovil(string marca,string placa,string modelo,string color,int capacidadtanquecombustible,int nivelcombustible,int rendimientocombustible)
+        {
+            Marca = marca;
+            Placa = placa;
+            Modelo = modelo;
+            Color = color;
+            CapacidadTanqueCombustible = capacidadtanquecombustible;
+            NivelCombustible = nivelcombustible;
+            RendimientoCombustible = rendimientocombustible;
+        }
+        public bool Conducir(int Distancia) {
+            double CombustibleNecesarario = Distancia / RendimientoCombustible;
+            return CombustibleNecesarario <= NivelCombustible;
+        }
+
+        public double Rellenar(double cantidad)
+        {
+            if (NivelCombustible + cantidad > CapacidadTanqueCombustible)
+            {
+                Console.WriteLine("No puede");
+            }
+            else {
+                Console.WriteLine("Si puede");
+            }
+           return NivelCombustible+= cantidad;
+        }
+        public int PuedeConducir(int Distancia) {
+            if (Distancia == 0)
+            {
+                Console.WriteLine("puede conducir");
+                NivelCombustible -= NivelCombustible;
+            }
+            else {
+                Console.WriteLine("No puede conducir");
+            }
+        return Distancia;
+        }
+    }
+}
---
+++ Automoviles/Automoviles/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Automoviles
+{
+    partial class Automovil
+    {
+        static void Main(string[] args)
+        {
+
+            Automovil automovil = new Automovil("Toyota ","M45615","Supra","Rojo",100,0,900);
+
+            Console.WriteLine($"Marca: {automovil.Marca}");
+            Console.WriteLine($"Placa: {automovil.Placa}");
+            Console.WriteLine($"Marca: {automovil.Modelo}");
+            Console.WriteLine($"Placa: {automovil.Color}");
+            Console.WriteLine($"Capacidad Del Tanque de Combustible: {automovil.CapacidadTanqueCombustible}");
+            Console.WriteLine($"Nivel del Combustible: {automovil.NivelCombustible}");
+            Console.WriteLine($"Rendimiento del Combustible: {automovil.RendimientoCombustible}");
+
+            int distancia = 900;
+
+            if (automovil.Conducir(distancia))
+            {
+
+                Console.WriteLine($"Puede Conducir esa distancia {distancia}");
+            }
+            else {
+                Console.WriteLine($"No puede conducir cierta distancia {distancia}");
+            }
+
+            double cantidadDeRellenar = 30.00;
+            automovil.Rellenar(cantidadDeRellenar);
+            Console.WriteLine($"Se relleno el tanque de combustible: {cantidadDeRellenar}");
+            Console.WriteLine($"Nivel de combustible actual despues de rellenarlo: {automovil.NivelCombustible}");
+
+
+        }
+    }
+}
